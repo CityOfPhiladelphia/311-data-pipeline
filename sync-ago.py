adcaf6405cd5227307fd6251c7798bcff1a50e6a
vespinoza0 <victor.espinoza@phila.gov.com>
2024-02-26 10:04:05 -0500

    print ago item id temp

--- sync-ago.py
+++ sync-ago.py
@@ -40,7 +40,8 @@ def sync(day):
 
     print('Connected to AGO.\n')
     
-    salesforce_creds = citygeo_secrets.connect_with_secrets(connect_salesforce, "SalesForce API" )
+    salesforce_creds = citygeo_secrets.connect_with_secrets(connect_salesforce, "SalesForce API_test" )
+    print('''salesforce_creds.get('AGO_item_id') ''', salesforce_creds.get('AGO_item_id'))
     flayer = org.content.get(salesforce_creds.get('AGO_item_id'))
     LAYER_OBJECT = flayer.layers[0]
     print(LAYER_OBJECT)
