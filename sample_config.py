8bbb1a68f35602e4748cf77a9a577fc37640a9f3
floptical <stephen.macdavid@gmail.com>
2024-03-14 13:17:15 -0400

    update sample config

--- sample_config.py
+++ sample_config.py
@@ -4,19 +4,63 @@ TEST=False
 LOG_LEVEL               = 'INFO'
 LOG_PATH                = './logs/sync.log'
 
-# Secrets for sync-ago.py
-MAPS_PASSWORD = 'password'
-SALESFORCE_AGO_ITEMID = '1234abcdf'
-DATABRIDGE_SDE_PASSWORD = 'password'
-DATABRIDGE_HOST = 'database.host'
-DATABRIDGE_DB = 'database'
+# Secret for sync-ago.py
+def get_salesforce_ago_layer(creds:dict): 
+    salesforce_creds=creds['salesforce API copy']
+    ago_creds =  citygeo_secrets.connect_with_secrets(get_ago_creds, "maps.phl.data")
+    org =GIS(url=ago_creds.get('url'),
+                username=ago_creds.get('login'),
+                password=ago_creds.get('password'),
+                verify_cert=False)
+    flayer = org.content.get(salesforce_creds.get('AGO_item_id'))
+    return flayer
 
-THREEONEONE_PASSWORD = 'password'
+# Secret for sync-ago.py
+def connect_sde(creds: dict):
+    db_creds = creds['SDE']
+    db_creds_2 = creds['databridge-oracle/hostname']
+    # Connect to database
+    dsn = cx_Oracle.makedsn(db_creds_2.get('host').get('hostName'), db_creds_2.get('host').get('port'),
+                                   service_name=db_creds_2.get('database'))
+    connection = cx_Oracle.connect(db_creds.get('login'), db_creds.get('password'), dsn,
+                                          encoding="UTF-8")
+    return connection
+
+# Secret for sync.py
+def connect_311(creds: dict):
+    db_creds = creds['GIS_311']
+    db_creds_2 = creds['databridge-oracle/hostname']
+    # Connect to database
+    dsn = cx_Oracle.makedsn(db_creds_2.get('host').get('hostName'), db_creds_2.get('host').get('port'),
+                                   service_name=db_creds_2.get('database'))
+    connection = cx_Oracle.connect(db_creds.get('login'), db_creds.get('password'), dsn,
+                                          encoding="UTF-8")
+    return connection
+
+# Secret for sync.py
+def connect_311_test(creds: dict):
+    db_creds = creds['GIS_311']
+    db_creds_2 = creds['databridge-oracle/hostname-testing']
+    # Connect to database
+    dsn = cx_Oracle.makedsn(db_creds_2.get('host').get('hostName'), db_creds_2.get('host').get('port'),
+                                   service_name=db_creds_2.get('database'))
+    connection = cx_Oracle.connect(db_creds.get('login'), db_creds.get('password'), dsn,
+                                          encoding="UTF-8")
+    return connection
+
+# Secret for sync.py
+def connect_salesforce_DEV(creds:dict):
+    salesforce_creds=creds['salesforce API copy']
+    sf = Salesforce(username=salesforce_creds.get('login'), \
+                       password=salesforce_creds.get('password'), \
+                       security_token=salesforce_creds.get('token'))
+    return sf
+
+
 
 # These describe the destination (enterprise) dataset.
 # These describe the destination (enterprise) dataset.
-DEST_PROD_DSN = ''
-DEST_TEST_DSN = ''
+
 DEST_DB_ACCOUNT         = ''
 DEST_TABLE              = 'SALESFORCE_CASES'
 DEST_UPDATED_FIELD      = 'updated_datetime'
@@ -61,10 +105,7 @@ FIELD_MAP = {
 # Microsoft Teams Web Connector URL
 MSTEAMS_CONNECTOR             = 'https://phila.webhook.office.com/webhookb2/763c9a83-0f38-4eb2-abfc-e0f2f41b6fbb@2046864f-68ea-497d-af34-a6629a6cd700/IncomingWebhook/434dfb8c116d472f8f224cfae367cdc1/2f82d684-85a4-4131-95a3-3342e012faeb'
 
-# These are for querying Salesforce.
-SF_USER             = ''
-SF_PASSWORD         = ''
-SF_TOKEN            = ''
+
 # Most of the filtering for the public view we do in the database, but the
 # `Type` field is not part of the schema, so we have to filter those cases
 # when querying Salesforce.
