cb1ed4701cc52a5179e5e618ef7007765f4441b7
floptical <stephen.macdavid@gmail.com>
2025-02-21 19:33:26 +0000

    partial update ad-hoc scripts

---
+++ temp-update.sh
@@ -0,0 +1,30 @@
+cd /scripts/311-data-pipeline/
+source ./venv/bin/activate
+
+python sync-db2-ago.py --prod --day 2024-03-15
+python sync-db2-ago.py --prod --day 2024-03-16
+python sync-db2-ago.py --prod --day 2024-03-17
+python sync-db2-ago.py --prod --day 2024-03-18
+python sync-db2-ago.py --prod --day 2024-03-19
+python sync-db2-ago.py --prod --day 2024-03-20
+python sync-db2-ago.py --prod --day 2024-03-21
+python sync-db2-ago.py --prod --day 2024-03-22
+python sync-db2-ago.py --prod --day 2024-03-23
+python sync-db2-ago.py --prod --day 2024-03-24
+python sync-db2-ago.py --prod --day 2024-03-25
+python sync-db2-ago.py --prod --day 2024-03-26
+python sync-db2-ago.py --prod --day 2024-03-27
+python sync-db2-ago.py --prod --day 2024-03-28
+python sync-db2-ago.py --prod --day 2024-03-29
+python sync-db2-ago.py --prod --day 2024-03-30
+python sync-db2-ago.py --prod --day 2024-03-31
+
+python sync-db2-ago.py --prod --day 2024-04-01
+python sync-db2-ago.py --prod --day 2024-04-02
+python sync-db2-ago.py --prod --day 2024-04-03
+python sync-db2-ago.py --prod --day 2024-04-04
+python sync-db2-ago.py --prod --day 2024-04-05
+python sync-db2-ago.py --prod --day 2024-04-06
+python sync-db2-ago.py --prod --day 2024-04-07
+python sync-db2-ago.py --prod --day 2024-04-08
+python sync-db2-ago.py --prod --day 2024-04-09
---
+++ update-by-months-sf.sh
@@ -0,0 +1,31 @@
+cd /scripts/311-data-pipeline/
+source ./venv/bin/activate
+
+python sync-db2.py --prod --month_refresh 2022-01
+python sync-db2.py --prod --month_refresh 2022-02
+python sync-db2.py --prod --month_refresh 2022-03
+python sync-db2.py --prod --month_refresh 2022-04
+python sync-db2.py --prod --month_refresh 2022-05
+python sync-db2.py --prod --month_refresh 2022-06
+python sync-db2.py --prod --month_refresh 2022-07
+python sync-db2.py --prod --month_refresh 2022-08
+python sync-db2.py --prod --month_refresh 2022-09
+python sync-db2.py --prod --month_refresh 2022-10
+python sync-db2.py --prod --month_refresh 2022-11
+python sync-db2.py --prod --month_refresh 2022-12
+python sync-db2.py --prod --month_refresh 2023-01
+python sync-db2.py --prod --month_refresh 2023-02
+python sync-db2.py --prod --month_refresh 2023-03
+python sync-db2.py --prod --month_refresh 2023-04
+python sync-db2.py --prod --month_refresh 2023-05
+python sync-db2.py --prod --month_refresh 2023-06
+python sync-db2.py --prod --month_refresh 2023-07
+python sync-db2.py --prod --month_refresh 2023-08
+python sync-db2.py --prod --month_refresh 2023-09
+python sync-db2.py --prod --month_refresh 2023-10
+python sync-db2.py --prod --month_refresh 2023-11
+python sync-db2.py --prod --month_refresh 2023-12
+python sync-db2.py --prod --month_refresh 2024-01
+python sync-db2.py --prod --month_refresh 2024-02
+python sync-db2.py --prod --month_refresh 2024-03
+python sync-db2.py --prod --month_refresh 2024-04
