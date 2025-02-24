184470e69d0fbe9b2cb4a29853ec1dd45a2e863d
floptical <stephen.macdavid@gmail.com>
2025-02-24 19:58:18 +0000

    updated_datetime, not requested_datetime

--- sync-db2-viewer.py
+++ sync-db2-viewer.py
@@ -11,7 +11,7 @@ def main(prod):
     conn = citygeo_secrets.connect_with_secrets(connect_databridge, 'databridge-v2/postgres', 'databridge-v2/hostname', 'databridge-v2/hostname-testing', prod=prod)
     try:
         # Update viewer_philly311.salesforce_cases from citygeo.salesforce_cases with
-        # ONLY the rows that are missing based off the requested_datetime column
+        # ONLY the rows that are missing based off the updated_datetime column
         # We could do a simple TRUNCATE and then select * to insert everything, but that's CPU intensive.
         # Only insert what we need.
         update_query = '''
@@ -29,8 +29,8 @@ def main(prod):
         media_url, lat, lon, subject, type_, description,
         description_full, private_case, sde.next_rowid('viewer_philly311','salesforce_cases')
         FROM citygeo.salesforce_cases rawview
-        WHERE rawview.requested_datetime > (
-            SELECT COALESCE(MAX(requested_datetime), '1970-01-01')
+        WHERE rawview.updated_datetime > (
+            SELECT COALESCE(MAX(updated_datetime), '1970-01-01')
             FROM viewer_philly311.salesforce_cases
         );
         '''
